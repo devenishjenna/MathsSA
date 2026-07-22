c9614d089dfd521ba3330407814bda745b457a70
Jenna Devenish <jennadevenish@Jennas-MacBook-Pro.local>
2026-07-22 12:18:07 +0200

    added static status circles of tabs

--- mathssa/app/components/Tabs.tsx
+++ mathssa/app/components/Tabs.tsx
@@ -13,13 +13,24 @@ export default function Tabs({ activeTab, topicSlug, gradeSlug }: TabsProps) {
 return <div className="flex">
     {tabs.map((tab) => 
       <Link key={tab} href={`/${gradeSlug}/${topicSlug}?tab=${tab.toLowerCase()}`}
-          className={`uppercase px-4 py-2 text-sm font-semibold tracking-widest cursor-pointer -mb-px rounded-t ${
+          className={`group uppercase px-4 py-2 text-sm font-semibold tracking-widest cursor-pointer -mb-px rounded-t ${
             activeTab === tab.toLowerCase()
               ? 'text-brand-blue bg-content'
-              : 'border-transparent text-gray-500 hover:text-gray-700'}
-          `}
+              : 'border-transparent text-gray-500 hover:text-gray-600'}
+              `}
       >
+        <div className="flex gap-2 justify-center items-center">
         {tab}
+        <span className={`w-3.5 h-3.5 rounded-full flex items-center justify-center
+          ${true // TODO: iscomplete
+            ? `bg-brand-green border-brand-green ${activeTab === tab.toLowerCase() ? '' : 'group-hover:bg-brand-green/60'}`
+            : 'border-current opacity-50'}`}
+          >
+          {true//TODO: iscomplete
+          && <span className="text-white text-[8px] leading-none">✓</span>}
+        </span>
+        
+        </div>
       </Link>
     )}
   
